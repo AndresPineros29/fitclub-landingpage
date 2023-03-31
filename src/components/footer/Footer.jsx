721cec9a81e2196f50e082ff5264476c764f34e5
AndresPineros29 <andespipa29@gmail.com>
2023-03-31 13:17:40 -0500

    links update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,8 +7,20 @@ const Footer = () => {
     <div className="footer-container">
       <div className="footer">
         <div className="social-links">
-          <img src={github} alt="github_icon" />
-          <img src={linkedin} alt="linkedin_icon" />
+          <a
+            href="https://github.com/AndresPineros29/fitclub-landingpage"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={github} alt="github_icon" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/andres-felipe-pi%C3%B1eros-pabon-0b1524264/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={linkedin} alt="linkedin_icon" />
+          </a>
         </div>
         <div className="logo-f">
           <img src={logo} alt="logo" />
